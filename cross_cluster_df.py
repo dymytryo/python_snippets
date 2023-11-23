49070c1e371ec9d063023e6a209b5a2a5aa8d55f
Dmytro Valiaiev <91760255+dymytryo@users.noreply.github.com>
2023-11-23 02:07:07 -0600

    Update cross_cluster_df.py

--- cross_cluster_df.py
+++ cross_cluster_df.py
@@ -4,49 +4,66 @@ import networkx as nx
 def cross_cluster(dataframe: pd.DataFrame, column_a: str, column_b: str, additional_columns: list) -> pd.DataFrame:
     """
     Create clusters based on the connections between two types of entities in a given DataFrame.
-    Each name in optional argument additional_columns should have a 1-to-1 relationship with column_b.
-    
+    Additionally, aggregates information from additional columns specified.
+
     Args:
         dataframe (pd.DataFrame): The DataFrame containing the relationship data.
-        column_a (str): The name of the first column in the DataFrame representing the first entity.
-        column_b (str): The name of the second column in the DataFrame representing the second entity.
-        additional_columns (list): List of names of additional columns that need to be aggregated. 
+        column_a (str): The name of the first column representing the first entity.
+        column_b (str): The name of the second column representing the second entity.
+        additional_columns (list): List of names of additional columns to be aggregated.
 
     Returns:
         pd.DataFrame: A DataFrame where each row represents a cluster with two lists of entity IDs from each column.
+        Additionally, includes aggregated data from specified additional columns.
 
     Raises:
         ValueError: If the specified columns are not found in the DataFrame.
 
     Example:
         >>> df = pd.DataFrame({'employeeId': ['E1', 'E2', 'E2'], 'departmentId': ['D5', 'D5', 'D6']})
-        >>> create_clusters(df, 'employeeId', 'departmentId', [])
+        >>> cross_cluster(df, 'employeeId', 'departmentId', [])
     """
+    # Check if specified columns are in the dataframe
     if column_a not in dataframe.columns or column_b not in dataframe.columns or any(col not in dataframe.columns for col in additional_columns):
         raise ValueError("One or more specified columns not found in the DataFrame")
 
+    # Initialize a graph to represent connections
     G = nx.Graph()
+
+    # Initialize a dictionary to hold additional information
     additional_info = {col: {} for col in additional_columns}
 
+    # Iterate over each row in the dataframe
     for _, row in dataframe.iterrows():
+        # Extract entity IDs
         entity_a_id = str(row[column_a])
         entity_b_id = str(row[column_b])
 
+        # Create nodes for graph based on entities
         entity_a_node = f'a{entity_a_id}'
         entity_b_node = f'b{entity_b_id}'
-        G.add_edge(entity_a_node, entity_b_node)
+        G.add_edge(entity_a_node, entity_b_node)  # Add an edge between these entities in the graph
 
-        # Collect data from additional columns
+        # Store additional data with both entity_a_id and entity_b_id as keys
         for col in additional_columns:
-            additional_info[col][entity_b_id] = row[col]
+            if (entity_a_id, entity_b_id) not in additional_info[col]:
+                # Initialize as a list with the first element
+                additional_info[col][(entity_a_id, entity_b_id)] = [row[col]]
+            else:
+                # Append to the existing list for duplicates
+                additional_info[col][(entity_a_id, entity_b_id)].append(row[col])
 
+    # Find connected components in the graph, each representing a cluster
     connected_components = list(nx.connected_components(G))
     clusters = []
 
+    # Process each connected component to form a cluster
     for component in connected_components:
+        # Extract entities of type A and B from the component
         entities_a = {n[1:] for n in component if n.startswith('a')}
         entities_b = {n[1:] for n in component if n.startswith('b')}
 
+        # Initialize a dictionary to represent the cluster
         cluster = {
             f'{column_a}s': list(entities_a),
             f'{column_b}s': list(entities_b)
@@ -54,11 +71,18 @@ def cross_cluster(dataframe: pd.DataFrame, column_a: str, column_b: str, additio
 
         # Aggregate additional column data
         for col in additional_columns:
-            col_data = [additional_info[col][entity] for entity in entities_b if entity in additional_info[col]]
+            col_data = []
+            # Iterate through all pairs of entity A and B
+            for a_id in entities_a:
+                for b_id in entities_b:
+                    # Check if the pair exists in additional info and append its data
+                    if (a_id, b_id) in additional_info[col]:
+                        col_data += additional_info[col][(a_id, b_id)]
             cluster[f'{column_b}_{col}'] = col_data
 
         clusters.append(cluster)
 
+    # Return the clusters as a DataFrame
     return pd.DataFrame(clusters)
 
 # Sample data
